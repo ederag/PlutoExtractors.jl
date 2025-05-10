c591500e98797074a549e464ab0b56a0daddc3e2
ederag <edera@gmx.fr>
2025-05-10 11:47:05 +0200

    Demonstrate the semicolon failure

---
+++ test/notebooks/source_semicolon.jl
@@ -0,0 +1,30 @@
+### A Pluto.jl notebook ###
+# v0.20.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 588ca7b5-0be6-42b1-95ef-850754832ad8
+import Pkg
+
+# ╔═╡ d54f6eae-833b-4337-bff7-c0e851b4b5af
+# ╠═╡ skip_as_script = true
+#=╠═╡
+Pkg.activate(Base.current_project())
+  ╠═╡ =#
+
+# ╔═╡ 5460ab14-ea6f-11ec-083c-339d0912f969
+a = 1
+
+# ╔═╡ 1b45d930-ffb8-48b1-974e-29e3257e0633
+b = 2a;
+
+# ╔═╡ 73585a4f-c88d-4221-bae9-dbd7e1f927e5
+c = 2b
+
+# ╔═╡ Cell order:
+# ╠═588ca7b5-0be6-42b1-95ef-850754832ad8
+# ╠═d54f6eae-833b-4337-bff7-c0e851b4b5af
+# ╠═5460ab14-ea6f-11ec-083c-339d0912f969
+# ╠═1b45d930-ffb8-48b1-974e-29e3257e0633
+# ╠═73585a4f-c88d-4221-bae9-dbd7e1f927e5
