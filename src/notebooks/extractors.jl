e8904601013eab076258b33710cd217cddb764ae
ederag <edera@gmx.fr>
2025-05-23 21:49:58 +0200

    Update docstrings
    Can't fight Pluto shuffle of code, but only the docstrings changed.

--- src/notebooks/extractors.jl
+++ src/notebooks/extractors.jl
@@ -49,10 +49,17 @@ end
 
 # ╔═╡ 7e8a7524-1ae6-439d-98c6-5b2390014096
 """
-    load_updated_topology(path)
+    load_updated_topology(
+        path;
+        get_code_str = c -> c.code,
+        get_code_expr = c -> Meta.parse(c.code)
+    )
 
 Return the topology of a `Pluto` notebook found at `path`,
 with references and definitions filled.
+`get_code_str` and `get_code_expr` are passed to
+[`PlutoDependencyExplorer.updated_topology`](@ref);
+see that function doc for more details.
 
 See also [`@nb_extract`](@ref)
 """
@@ -90,6 +97,189 @@ function template_analysis(template::Expr)
 	node = EE.compute_reactive_node(template_body)
 	needed_symbols = filter(∉(given_symbols), node.references)
 	dict, given_symbols, needed_symbols
+end
+
+# ╔═╡ ba256080-73fb-4de4-be72-101318c82029
+strip_types(x::Symbol) = x
+
+# ╔═╡ feadac3a-859c-4915-bfc6-8fa607d6b606
+# This function is used to be able to strip types from the extracted arguments to forward the function call to the gensymed one
+# We want to do something like `fun(a::Something) = MODULE.#xxx#fun(a)`
+function strip_types(x::Expr)::Symbol
+	if Meta.isexpr(x, [:(::), :kw])
+		return strip_types(x.args[1]) # We recursively call strip_types till we reach the symbol or we error
+	else
+		error("This function should only receive either symbols or Expr of the type `x::Something`, `x=something` or `x::Something=something`. Instead `$x` was given as input")
+	end
+end
+
+# ╔═╡ 5c15516e-e3e1-401b-97b5-f4ce60e3a234
+function fun_wrapper(template_dict, module_sym::Symbol)
+	# Wrapper to be evaluated in the caller scope,
+	# that will call the module function
+	# Adapted from https://fluxml.ai/MacroTools.jl/dev/utilities/
+	wrapper_dict = copy(template_dict)
+	wrapper_dict[:body] = :(
+		$(module_sym).$(template_dict[:name])(
+			$(strip_types.(template_dict[:args])...);
+			$(strip_types.(template_dict[:kwargs])...)
+		)
+	)
+	MacroTools.combinedef(wrapper_dict)
+end
+
+# ╔═╡ c2f701da-aaa7-4af5-bada-5acb05465b3f
+"""
+    @nb_extract(utp, template)
+
+Create a function out of a `Pluto` notebook updated topology,
+based on a template.
+
+The notebook updated topology `utp` must be a `PlutoDependencyExplorer.NotebookTopology{Pluto.Cell}`,
+with up-to-date topology, filled with references and definitions
+(for instance from [`load_updated_topology`](@ref)).
+
+The signature of the returned function is exactly the one of the `template`.
+The arguments take precedence over the values defined in the notebook.
+
+The updated topology `utp` and the template are then analyzed
+to find the cells that are necessary to evaluate the template body
+(the cells that define `b` and `c` in the last example).
+
+Those cells code is prepended to the template body, to form the function body.
+
+The result of the macro is this fleshed-out function,
+just as if it had been typed by hand.
+Preliminary benchmarks show no overhead.
+
+Remark: `@nb_extract` can be used not only from a running Pluto notebook,
+but from anywhere else (a script, the REPL, ...).
+
+# Examples
+Say in the source notebook there are three cells: `a = 1`, `b = 2a`, `c = 2b`,
+here is how to make a function that return the value `c` from any given `a`:
+```jldoctest
+julia> using PlutoExtractors: load_updated_topology, @nb_extract
+julia> source_path = pkgdir(PlutoExtractors,
+	"test", "notebooks", "source_basic.jl"
+)  # to be replaced with the path of your source notebook
+julia> utp = load_updated_topology(source_path);
+julia> @nb_extract(
+	utp,
+	function fun(a)
+		return c
+	end
+)
+julia> fun(2)
+8
+```
+
+See also [`load_updated_topology`](@ref)
+"""
+macro nb_extract(utp, template)
+	# will put the function definition inside a module,
+	# so that the necessary packages are accessible
+	# both for the macroexpansion phase (to determine dependencies)
+	# and for the function
+	module_sym = gensym(:PlutoExtract)
+
+	(template_dict, given_symbols, needed_symbols) = template_analysis(template)
+
+	# The wrapper expression has to be build beforehand,
+	# to be put at the end of the quote,
+	# to be evaluated in the local scope rather than in toplevel
+	fun_wrapper_expr = fun_wrapper(
+		template_dict,
+		module_sym
+	)
+
+	# `nb_extractor_body` needs to know about the real notebook
+	# so the following can only be done at runtime.
+	# => Just prepare the expressions to be evaluated when the macro is executed.
+	return quote
+		let
+			# utp is not complete yet, but enough to gather the module header
+			header = gather_header($(esc(utp)), $(QuoteNode(given_symbols)))
+			module_expr = get_module_expr(
+				$(QuoteNode(module_sym)),
+				$(esc(utp)),
+				header,
+			)
+			m = $__module__.eval(module_expr)
+
+			# Now can use the utp of the module
+			# (more complete thanks to the macroexpansion,
+			#  that succeeds because the packages are available inside the module)
+			types_expr, fun_expr = nb_extractor_body(
+				m.utp,
+				$(QuoteNode(template_dict)),
+				$(QuoteNode(given_symbols))
+			)
+
+			# Put in the module the "types" expressions (`struct` for instance)
+			# (not allowed in the function)
+			for expr in types_expr
+				Base.eval(m, expr)
+			end
+
+			Base.eval(m, fun_expr)
+		end
+		# This last expression will be evaluated in the caller's scope
+		$(esc(fun_wrapper_expr))
+	end
+end
+
+
+# ╔═╡ 7fe4dc9a-9821-4924-bacb-0cebae1e74bd
+function fake_bind_expr()
+	quote
+		# Code reformatted from Pluto submodule PlutoRunner/src/bonds.jl fakebind
+		# (the one that Pluto inserts in the .jl file)
+		macro bind(def, element)
+		    return quote
+		        local iv = try
+						Base.loaded_modules[
+							Base.PkgId(
+								Base.UUID("6e696c72-6542-2067-7265-42206c756150"),
+								"AbstractPlutoDingetjes"
+							)
+						].Bonds.initial_value
+					catch
+						b -> missing
+					end
+		        local el = $(esc(element))
+		        global $(esc(def)) = Core.applicable(Base.get, el) ?
+					Base.get(el) :
+					iv(el)
+		        el
+		    end
+		end
+	end
+end
+
+# ╔═╡ 56764600-5efa-45bd-bf9e-68dae3bde72c
+function gather_header(utp, given)
+	expressions = Expr[]
+	for cell in utp.cell_order
+		expr = Meta.parse(cell.code)
+		node = utp.nodes[cell]
+
+		# Pluto allows to write markdown strings
+		# without an explicit `using Markdown`.
+		if Symbol("@md_str") in node.macrocalls
+			push!(expressions, :(using Markdown))
+		end
+
+		# Need all usings and imports
+		# to create the full m.utp (with macroexpansion).
+		usings_imports = EE.compute_usings_imports(expr)
+		append!(expressions, usings_imports.usings)
+		append!(expressions, usings_imports.imports)
+	end
+	# Needed for update_with_macroexpand to succeed, if any @bind
+	expr = fake_bind_expr()
+	push!(expressions, expr)
+	expressions
 end
 
 # ╔═╡ 9194537f-8d42-422b-afa2-f86933522efc
@@ -154,6 +344,17 @@ function all_needed_cells(
 	needed
 end
 
+# ╔═╡ 63c26add-ac5a-4a2c-9779-bd6c9710fe1a
+function remove_trailing_semicolon(str)
+	rstrip(c -> c == ';' || isspace(c), str)
+end
+
+# ╔═╡ e4ecb782-85af-4e66-a7af-72eca79bd191
+function has_usings_imports(ex)
+	(; usings, imports) = EE.compute_usings_imports(ex)
+	!isempty(usings) || !isempty(imports)
+end
+
 # ╔═╡ da3fb260-a858-457f-8430-c6a124d1d1e5
 function gather_bind_symbols(ex)
 	symbols = Symbol[]
@@ -171,155 +372,6 @@ function gather_bind_symbols(ex)
 		end
 	end
 	symbols
-end
-
-# ╔═╡ ba256080-73fb-4de4-be72-101318c82029
-strip_types(x::Symbol) = x
-
-# ╔═╡ feadac3a-859c-4915-bfc6-8fa607d6b606
-# This function is used to be able to strip types from the extracted arguments to forward the function call to the gensymed one
-# We want to do something like `fun(a::Something) = MODULE.#xxx#fun(a)`
-function strip_types(x::Expr)::Symbol
-	if Meta.isexpr(x, [:(::), :kw])
-		return strip_types(x.args[1]) # We recursively call strip_types till we reach the symbol or we error
-	else
-		error("This function should only receive either symbols or Expr of the type `x::Something`, `x=something` or `x::Something=something`. Instead `$x` was given as input")
-	end
-end
-
-# ╔═╡ 5c15516e-e3e1-401b-97b5-f4ce60e3a234
-function fun_wrapper(template_dict, module_sym::Symbol)
-	# Wrapper to be evaluated in the caller scope,
-	# that will call the module function
-	# Adapted from https://fluxml.ai/MacroTools.jl/dev/utilities/
-	wrapper_dict = copy(template_dict)
-	wrapper_dict[:body] = :(
-		$(module_sym).$(template_dict[:name])(
-			$(strip_types.(template_dict[:args])...);
-			$(strip_types.(template_dict[:kwargs])...)
-		)
-	)
-	MacroTools.combinedef(wrapper_dict)
-end
-
-# ╔═╡ c2f701da-aaa7-4af5-bada-5acb05465b3f
-"""
-    @nb_extract(utp, template)
-
-Create a function out of a `Pluto` notebook updated topology,
-based on a template.
-
-The notebook updated topology `utp` must be a `PlutoDependencyExplorer.NotebookTopology{Pluto.Cell}`,
-with up-to-date topology, filled with references and definitions
-(for instance from [`load_updated_topology`](@ref)).
-
-The signature of the returned function is exactly the one of the `template`.
-The argument names must match symbols defined in the notebook.
-The arguments values will be used instead of the source notebook ones
-in the evaluation.
-
-The return value of this function is found in the `template` body.
-This body must consist of a single `return` statement.
-The return value can be a single variable (e.g. ` return c`),
-a tuple (e.g. `return (b, c)`), or a named tuple (e.g. `return (; b = b, whatever = c)`).
-
-The updated topology `utp` is then analyzed to find the cells that are necessary
-to evaluate the return value
-(the cells that define `a` and `b` in the last examples).
-
-Those cells code is used as the core of the function body.
-
-The result of the macro is this fleshed-out function,
-just as if it had been typed by hand.
-Preliminary benchmarks show no overhead.
-
-Remark: `@nb_extract` can be used not only from a running Pluto notebook,
-but from anywhere else (a script, the REPL, ...).
-
-# Examples
-Say in the source notebook there are three cells: `a = 1`, `b = 2a`, `c = 2b`,
-here is how to make a function that return the value `c` from any given `a`:
-```jldoctest
-julia> using PlutoExtractors: load_updated_topology, @nb_extract
-julia> source_path = pkgdir(PlutoExtractors,
-	"test", "notebooks", "source_basic.jl"
-)  # to be replaced with the path of your source notebook
-julia> utp = load_updated_topology(source_path);
-julia> @nb_extract(
-	utp,
-	function fun(a)
-		return c
-	end
-)
-julia> fun(2)
-8
-```
-
-See also [`load_nb_with_topology`](@ref)
-"""
-macro nb_extract(utp, template)
-	# will put the function definition inside a module,
-	# so that the necessary packages are accessible
-	# both for the macroexpansion phase (to determine dependencies)
-	# and for the function
-	module_sym = gensym(:PlutoExtract)
-
-	(template_dict, given_symbols, needed_symbols) = template_analysis(template)
-
-	# The wrapper expression has to be build beforehand,
-	# to be put at the end of the quote,
-	# to be evaluated in the local scope rather than in toplevel
-	fun_wrapper_expr = fun_wrapper(
-		template_dict,
-		module_sym
-	)
-
-	# `nb_extractor_body` needs to know about the real notebook
-	# so the following can only be done at runtime.
-	# => Just prepare the expressions to be evaluated when the macro is executed.
-	return quote
-		let
-			# utp is not complete yet, but enough to gather the module header
-			header = gather_header($(esc(utp)), $(QuoteNode(given_symbols)))
-			module_expr = get_module_expr(
-				$(QuoteNode(module_sym)),
-				$(esc(utp)),
-				header,
-			)
-			m = $__module__.eval(module_expr)
-
-			# Now can use the utp of the module
-			# (more complete thanks to the macroexpansion,
-			#  that succeeds because the packages are available inside the module)
-			types_expr, fun_expr = nb_extractor_body(
-				m.utp,
-				$(QuoteNode(template_dict)),
-				$(QuoteNode(given_symbols))
-			)
-
-			# Put in the module the "types" expressions (`struct` for instance)
-			# (not allowed in the function)
-			for expr in types_expr
-				Base.eval(m, expr)
-			end
-
-			Base.eval(m, fun_expr)
-		end
-		# This last expression will be evaluated in the caller's scope
-		$(esc(fun_wrapper_expr))
-	end
-end
-
-
-# ╔═╡ 63c26add-ac5a-4a2c-9779-bd6c9710fe1a
-function remove_trailing_semicolon(str)
-	rstrip(c -> c == ';' || isspace(c), str)
-end
-
-# ╔═╡ e4ecb782-85af-4e66-a7af-72eca79bd191
-function has_usings_imports(ex)
-	(; usings, imports) = EE.compute_usings_imports(ex)
-	!isempty(usings) || !isempty(imports)
 end
 
 # ╔═╡ 58780697-0b89-420c-8b22-a31705ce45e4
@@ -383,58 +435,6 @@ function nb_extractor_body(utp, template_dict::Dict, given_symbols)
 	fun_expr = MacroTools.combinedef(new_dict)
 	
 	return types_expr, fun_expr
-end
-
-# ╔═╡ 7fe4dc9a-9821-4924-bacb-0cebae1e74bd
-function fake_bind_expr()
-	quote
-		# Code reformatted from Pluto submodule PlutoRunner/src/bonds.jl fakebind
-		# (the one that Pluto inserts in the .jl file)
-		macro bind(def, element)
-		    return quote
-		        local iv = try
-						Base.loaded_modules[
-							Base.PkgId(
-								Base.UUID("6e696c72-6542-2067-7265-42206c756150"),
-								"AbstractPlutoDingetjes"
-							)
-						].Bonds.initial_value
-					catch
-						b -> missing
-					end
-		        local el = $(esc(element))
-		        global $(esc(def)) = Core.applicable(Base.get, el) ?
-					Base.get(el) :
-					iv(el)
-		        el
-		    end
-		end
-	end
-end
-
-# ╔═╡ 56764600-5efa-45bd-bf9e-68dae3bde72c
-function gather_header(utp, given)
-	expressions = Expr[]
-	for cell in utp.cell_order
-		expr = Meta.parse(cell.code)
-		node = utp.nodes[cell]
-
-		# Pluto allows to write markdown strings
-		# without an explicit `using Markdown`.
-		if Symbol("@md_str") in node.macrocalls
-			push!(expressions, :(using Markdown))
-		end
-
-		# Need all usings and imports
-		# to create the full m.utp (with macroexpansion).
-		usings_imports = EE.compute_usings_imports(expr)
-		append!(expressions, usings_imports.usings)
-		append!(expressions, usings_imports.imports)
-	end
-	# Needed for update_with_macroexpand to succeed, if any @bind
-	expr = fake_bind_expr()
-	push!(expressions, expr)
-	expressions
 end
 
 # ╔═╡ Cell order:
