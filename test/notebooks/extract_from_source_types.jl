b805c20d95ea29866230e08c2ab3b4cff7f19af3
ederag <edera@gmx.fr>
2025-05-24 11:55:05 +0200

    Remove unnecessary workaround

--- test/notebooks/extract_from_source_types.jl
+++ test/notebooks/extract_from_source_types.jl
@@ -40,24 +40,6 @@ md"## Types
 # ╔═╡ f3ee4c34-3c96-4254-aa07-34d67efaedc3
 utp = load_updated_topology(source_nb_file)
 
-# ╔═╡ fafc2698-d303-4ee8-a9d1-a8a1dc523d2b
-md"""
-#### Copied by hand (workaround)
-
-Types will need some special handling, as they must be toplevel
-(they can't be defined inside a function).
-Until then, they have to be copied by hand.
-"""
-
-# ╔═╡ 90e0bfa8-9b4c-4be0-9733-e32a7e0ca5c9
-abstract type AbstractPoint end
-
-# ╔═╡ e67c92e5-26b6-4a8a-9f07-f23e94c83dbb
-struct Point{F<:Real} <: AbstractPoint
-	x::F
-	y::F
-end
-
 # ╔═╡ e4837658-a2ef-4dfa-8cb0-f909eac426b6
 md"""
 ### Tests
@@ -107,9 +89,6 @@ PlutoTest.@test fun_2() === true
 # ╠═8411b188-54d9-43fd-9d47-6bd6d7f5ed86
 # ╠═53c808e9-a4bc-4fbc-b186-41532f54e7f8
 # ╠═f3ee4c34-3c96-4254-aa07-34d67efaedc3
-# ╠═fafc2698-d303-4ee8-a9d1-a8a1dc523d2b
-# ╠═90e0bfa8-9b4c-4be0-9733-e32a7e0ca5c9
-# ╠═e67c92e5-26b6-4a8a-9f07-f23e94c83dbb
 # ╠═e4837658-a2ef-4dfa-8cb0-f909eac426b6
 # ╠═525f5fcc-efd1-48ef-8aeb-9c184e54a50d
 # ╠═b8879c23-6aec-435a-a875-e30ebd1b4d5a
